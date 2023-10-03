4ce92ceb06868f95494c17805ae53e8189797c32
GeonHeeAhn <justamoment@ewhain.net>
2023-10-04 04:09:03 +0900

    style: photo adding modal

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import home from "../Assets/icons/home.png";
@@ -6,36 +6,63 @@ import map from "../Assets/icons/map.png";
 import friends from "../Assets/icons/friends.png";
 import settings from "../Assets/icons/settings.png";
 
+import { ModalBack, ModalBox, ModalBtn, ExitBtn } from "./PhotoModal";
+
 export const NavBar = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalBackground = useRef();
+
   return (
-    <NavigationBar>
-      <NavBtn>
-        <img src={home} alt="로고" />
-      </NavBtn>
-      <NavBtn>
-        <img src={friends} alt="로고" />
-      </NavBtn>
-      <NavBtn></NavBtn>
-      <NavBtn>
-        <img src={map} alt="로고" />
-      </NavBtn>
-      <NavBtn>
-        <img src={settings} alt="로고" />
-      </NavBtn>
-    </NavigationBar>
+    <>
+      <>
+        <CenterBtn onClick={() => setModalOpen(true)} />
+        {modalOpen && (
+          <ModalBack
+            ref={modalBackground}
+            onClick={(e) => {
+              if (e.target === modalBackground.current) {
+                setModalOpen(false);
+              }
+            }}
+          >
+            <ModalBox>
+              <ModalBtnTop>카메라</ModalBtnTop>
+              <ModalBtn>사진보관함</ModalBtn>
+            </ModalBox>
+            <ExitBtn onClick={() => setModalOpen(false)}>취소</ExitBtn>
+          </ModalBack>
+        )}
+      </>
+      <NavigationBar>
+        <NavBtn>
+          <img src={home} alt="로고" />
+        </NavBtn>
+        <NavBtn>
+          <img src={friends} alt="로고" />
+        </NavBtn>
+        <NavBtn></NavBtn>
+        <NavBtn>
+          <img src={map} alt="로고" />
+        </NavBtn>
+        <NavBtn>
+          <img src={settings} alt="로고" />
+        </NavBtn>
+      </NavigationBar>
+    </>
   );
 };
 
 export const NavigationBar = styled.div`
   bottom: 0;
-  height: 80px;
+  height: 94px;
   width: 100%;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
   box-shadow: 0px -5px 20px rgba(0, 0, 0, 0.07);
   background: white;
-  !postiton: fixed;
+  !postition: fixed;
+  z-index: 998;
 `;
 export const NavBtn = styled.button`
   width: 30px;
@@ -45,4 +72,24 @@ export const NavBtn = styled.button`
   align-items: center;
   aligin-content: center;
   border: none;
+  margin: 0px 0px 10px 0px;
+  padding: 0px;
+`;
+
+export const CenterBtn = styled.button`
+  position: fixed;
+  bottom: 50px;
+  margin: 0 auto;
+  width: 56px;
+  height: 56px;
+  border: none;
+  border-radius: 50%;
+  background: #ffc329;
+  z-index: 999;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.07);
+`;
+
+export const ModalBtnTop = styled(ModalBtn)`
+  //왜인지 모르겠으나 적용이 안댐..
+  border-bottom: 1px #dddddd;
 `;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
+
 import BackIcon from "../Assets/icons/goback.png";
 import dummy1 from "../Assets/dumy.png";
 
-import { NavBar, NavBtn } from "../Components/NavBar";
+import { NavBar, NavBtn, CenterBtn } from "../Components/NavBar";
 import BackgroundContainer from "../Components/BackgroundContainer";
 import Main from "../Components/Main";
 import {
@@ -11,43 +12,40 @@ import {
   TxtBox,
   TextArea,
 } from "../Components/postingComponents";
-import { TopBar, Title, Back } from "../Components/TopBar";
+import { TopBar, Title } from "../Components/TopBar";
 
-const Home = () => {
+export const Home = () => {
   return (
-    <div>
-      <BackgroundContainer>
-        <Main>
-          <TopBar>
-            <NavBtn>
-              <img src={BackIcon} alt="뒤로가기" />
-            </NavBtn>
-            <Title>네컷 등록</Title>
-          </TopBar>
-          <MiniContainer>
-            <img src={dummy1} />
-            <SmallerTitle>When</SmallerTitle>
-            <TxtBox type="text" placeholder="ex) 2023. 05. 11" />
-            <SmallerTitle>Where</SmallerTitle>
-            <TxtBox type="text" placeholder="이화여자대학교" />
-            <SmallerTitle>Whom</SmallerTitle>
-            <SmallerTitle>What</SmallerTitle>
-            <TextArea placeholder="내용을 입력해주세요" />
-          </MiniContainer>
-        </Main>
-        <NavBar />
-      </BackgroundContainer>
-    </div>
+    <BackgroundContainer>
+      <TopBar>
+        <NavBtn>
+          <img src={BackIcon} alt="뒤로가기" />
+        </NavBtn>
+        <Title>네컷 등록</Title>
+      </TopBar>
+      <MiniContainer>
+        <img src={dummy1} />
+        <SmallerTitle>When</SmallerTitle>
+        <TxtBox type="text" placeholder="ex) 2023. 05. 11" />
+        <SmallerTitle>Where</SmallerTitle>
+        <TxtBox type="text" placeholder="이화여자대학교" />
+        <SmallerTitle>Whom</SmallerTitle>
+        <SmallerTitle>What</SmallerTitle>
+        <TextArea placeholder="내용을 입력해주세요" />
+      </MiniContainer>
+      <NavBar />
+    </BackgroundContainer>
   );
 };
 
 export default Home;
 
-const MiniContainer = styled(Main)`
-  width: 70%;
+export const MiniContainer = styled(Main)`
+  width: 80%;
   display: flex;
   height: calc(100vh-140px);
   justify-content: center;
+  overflow-x: hidden;
   overflow-y: scroll;
   ::-webkit-scrollbar {
     display: none;
